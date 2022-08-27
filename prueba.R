8a1cf6ef38747872185b38166971edca7b6c47dc
sullivansantana <82044094+sullivansantana@users.noreply.github.com>
2022-08-27 10:11:28 -0500

    Create prueba.R

---
+++ prueba.R
@@ -0,0 +1,21 @@
+    
+  # ##############################################################
+  #               Taller de Github con R 
+  # ##############################################################
+  
+  
+  # Introducciona github
+  # 1. Instalar Git
+  # 2. Crear Repositorio desde GitHub
+  # 3. Crear un Proyecto con version control (Git)
+  # 4. Clonar repositorio a la computadora
+  
+  # Crear codigo
+  df <- mtcars
+  names(mtcars)
+  
+  summary(mtcars)
+  plot(df$hp, main = "Grafico")
+  
+  # modificacion
+  
